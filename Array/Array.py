6f2bff71fa5d3c98e4ec686280e68d07b35b7266
nayansurve <survenayan870@gmail.com>
2025-11-27 21:46:43 +0530

    Array

---
+++ Array/Array.py
@@ -0,0 +1,10 @@
+# Program 1: Input and print array
+arr = []
+
+n = int(input("Enter number of elements: "))
+
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+
+print("Your array is:", arr)
